71a8dcac71ec2e57247a7c572f5bf0565610ee8d
jgw9617 <jgw9617@gmail.com>
2020-05-23 22:33:00 -0700

    feat(): login first impl

--- src/script/components/header.ts
+++ src/script/components/header.ts
@@ -1,8 +1,12 @@
-import { LitElement, css, html, customElement } from 'lit-element';
+import { LitElement, css, html, customElement, property } from 'lit-element';
 
+import '@pwabuilder/pwaauth';
+import { set } from 'idb-keyval';
 
 @customElement('app-header')
 export class AppHeader extends LitElement {
+
+  @property({ type: String }) username: string | null = null;
 
   static get styles() {
     return css`
@@ -23,8 +27,36 @@ export class AppHeader extends LitElement {
         font-weight: bold;
       }
 
+      pwa-auth {
+        margin-right: 9em;
+      }
+
+      pwa-auth::part(signInButton) {
+        background: none;
+        border: solid 1px;
+        border-radius: 2px;
+        padding: 6px;
+        padding-left: 12px;
+        padding-right: 12px;
+        text-transform: uppercase;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      @media (max-width: 800px) {
+        pwa-auth {
+          margin-right: 0em;
+        }
+      }
+
       @media (prefers-color-scheme: dark) {
         header {
+          color: white;
+        }
+
+        pwa-auth::part(signInButton) {
           color: white;
         }
       }
@@ -35,10 +67,28 @@ export class AppHeader extends LitElement {
     super();
   }
 
+  async firstUpdated() {
+    this.shadowRoot?.querySelector('pwa-auth')?.addEventListener('signin-completed', async (e: any) => {
+      console.log(e.detail);
+
+      if (e.detail.providerData) {
+        await set('local', e.detail.providerData)
+      }
+
+      this.username = e.detail.name;
+    })
+  }
+
   render() {
     return html`
       <header>
         <h1>Memos</h1>
+
+        ${this.username && this.username.length > 1 ? html`<span>${this.username}</span>` : html`<pwa-auth 
+          microsoftkey="e7e2eedb-420c-4034-8f16-a6cd4e3c81b5"
+          credentialmode="none"
+          menuPlacement="end">
+        </pwa-auth>`}
       </header>
     `;
   }
